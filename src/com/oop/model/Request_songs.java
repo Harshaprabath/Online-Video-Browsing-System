8c804174abf6abac9d3ca1d40fd9323ba39b4418
sasanka <it20037956@my.sliit.lk>
2021-05-19 03:36:53 +0530

    # WARNING: head commit changed in the meantime
    changes sasanka

---
+++ src/com/oop/model/Request_songs.java
@@ -0,0 +1,40 @@
+package com.oop.model;
+
+public class Request_songs {
+	private int rsID;
+	private String songName;
+	private String singerName;
+	private String email;
+	public Request_songs(int rsID, String songName, String singerName, String email) {
+		
+		this.rsID = rsID;
+		this.songName = songName;
+		this.singerName = singerName;
+		this.email = email;
+	}
+	public int getRsID() {
+		return rsID;
+	}
+	public void setRsID(int rsID) {
+		this.rsID = rsID;
+	}
+	public String getSongName() {
+		return songName;
+	}
+	public void setSongName(String songName) {
+		this.songName = songName;
+	}
+	public String getSingerName() {
+		return singerName;
+	}
+	public void setSingerName(String singerName) {
+		this.singerName = singerName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+		
+}
