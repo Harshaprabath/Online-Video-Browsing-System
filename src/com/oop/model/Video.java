7b3981181400eb1aedbf35540f314dfa6f32a789
Harshaprabath <prabhathhemasir@gmail.com>
2021-05-19 05:09:29 +0530

    video model

--- src/com/oop/model/Video.java
+++ src/com/oop/model/Video.java
@@ -1,9 +1,55 @@
 package com.oop.model;
 
+import javax.servlet.http.Part;
+
 public class Video {
 	
 	private String videoId;
 	private String videoName;
 	private Part video;
+	
+	public Video(String videoId, String videoName, Part video) {
+		
+		this.videoId = videoId;
+		this.videoName = videoName;
+		this.video = video;
+	}
+	
+	public Video() {
+		
+		this.videoId = null;
+		this.videoName =null;
+		this.video = null;
+	}
 
+	public String getVideoId() {
+		
+		return videoId;
+	}
+
+	public void setVideoId(String videoId) {
+		
+		this.videoId = videoId;
+	}
+
+	public String getVideoName() {
+		
+		return videoName;
+	}
+
+	public void setVideoName(String videoName) {
+		
+		this.videoName = videoName;
+	}
+
+	public Part getVideo() {
+		
+		return video;
+	}
+
+	public void setVideo(Part video) {
+		
+		this.video = video;
+	}
+	
 }
