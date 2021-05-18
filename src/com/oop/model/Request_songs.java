0e74085dff9a344df57befeae27a96559de10d7b
sasanka <it20037956@my.sliit.lk>
2021-05-19 03:45:00 +0530

    req songs

--- src/com/oop/model/Request_songs.java
+++ src/com/oop/model/Request_songs.java
@@ -5,6 +5,8 @@ public class Request_songs {
 	private String songName;
 	private String singerName;
 	private String email;
+	
+	
 	public Request_songs(int rsID, String songName, String singerName, String email) {
 		
 		this.rsID = rsID;
@@ -12,27 +14,45 @@ public class Request_songs {
 		this.singerName = singerName;
 		this.email = email;
 	}
+	
+	public Request_songs() {
+		
+		this.rsID = 0;
+		this.songName = null;
+		this.singerName = null;
+		this.email = null;
+	}
+	
+	
+	
 	public int getRsID() {
 		return rsID;
 	}
+	
 	public void setRsID(int rsID) {
 		this.rsID = rsID;
 	}
+	
 	public String getSongName() {
 		return songName;
 	}
+	
 	public void setSongName(String songName) {
 		this.songName = songName;
 	}
+	
 	public String getSingerName() {
 		return singerName;
 	}
+	
 	public void setSingerName(String singerName) {
 		this.singerName = singerName;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
