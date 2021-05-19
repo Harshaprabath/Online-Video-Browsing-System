2c5e92e813157b8cada0c1d990aee906912d7b2e
Harshaprabath <prabhathhemasir@gmail.com>
2021-05-20 05:29:28 +0530

    register sevelet and database conectivity

---
+++ src/com/oop/util/RegisterDButil.java
@@ -0,0 +1,54 @@
+package com.oop.util;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import com.mysql.jdbc.Statement;
+import com.oop.DBconection.DataBaseConection;
+
+public class RegisterDButil {
+	private static Connection con=null;
+	private static Statement statement=null;
+	public static PreparedStatement st=null;
+	
+	public static boolean register(String firstName,String lastname,String address,String city,String country,String email,String password) {
+		
+		boolean isSuccess= false;
+		
+		try {
+			
+			con = DataBaseConection.getConnection();
+			String sql="INSERT INTO user values (0,?, ? , ? , ? , ? , ? ,?)";
+			
+			st=con.prepareStatement(sql);
+			
+			st.setString(1,firstName);
+			st.setString(2,lastname);
+			st.setString(3,address);
+			st.setString(4,city);
+			st.setString(5,country);
+			st.setString(6,email);
+			st.setString(7,password);
+			
+			int result = st.executeUpdate();
+			
+			if(result>0) {
+				
+				isSuccess=true;
+			}
+			else {
+				
+				isSuccess=false;
+			}
+		
+		}catch(Exception e) {
+			
+			e.printStackTrace();
+			
+		}
+		
+		return isSuccess;
+		
+	}
+	
+}
